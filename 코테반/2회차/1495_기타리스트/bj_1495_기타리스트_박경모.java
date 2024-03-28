144e424e9eee306f43c37ed7bf522c08c3c8f88b
Elenaljh <zerom980706@gmail.com>
2024-03-28 15:02:36 +0900

    코테반 2회차 : 기타리스트 (메모리초과), LCS

--- 코테반/2회차/1495_기타리스트/bj_1495_기타리스트_박경모.java
+++ 코테반/2회차/1495_기타리스트/bj_1495_기타리스트_박경모.java
@@ -68,3 +68,61 @@ public class Main {
 		
 	}
 }
+
+
+/* 풀이2 (메모리초과 : 큐 사용)
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class bj_1495_기타리스트 {
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		int[] V = new int[N];
+		for(int i=0; i<N; i++) {
+			V[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.offer(S); 
+		int cycle = 1; 
+		for(int i=0; i<N; i++) {
+			int cnt = 0; 
+			for(int j=0; j<cycle; j++) {
+				int temp = queue.poll();
+				if(temp - V[i] >= 0) {
+					queue.offer(temp - V[i]);
+					cnt++;
+				}
+				if(temp + V[i] <= M) {
+					queue.offer(temp + V[i]);
+					cnt++;
+				}
+			}
+			cycle = cnt;
+		}
+		int max = -1;
+		while(!queue.isEmpty()) {
+			int item = queue.poll();
+			max = (max>item) ? max : item;
+		}
+		System.out.println(max);
+	}
+}
+
+
+
+*/
--- 코테반/2회차/9251_LCS/bj_9251_LCS_박경모java.java
+++ 코테반/2회차/9251_LCS/bj_9251_LCS_박경모java.java
@@ -1,3 +1,38 @@
-public class bj_9251_LCS_박경모{
-    
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj_9251_LCS {
+	public static void main(String[] args) throws IOException {
+		System.setIn(new FileInputStream("input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s1 = br.readLine();
+		String s2 = br.readLine();
+		
+		int s1Len = s1.length();
+		int s2Len = s2.length();
+		
+		int[][] LCS = new int[s1Len+1][s2Len+1];
+		
+		for(int i =1; i<s1Len+1; i++) {
+			for(int j=1; j<s2Len+1; j++) {
+				if(s1.charAt(i-1) == s2.charAt(j-1)) {
+					LCS[i][j] = LCS[i-1][j-1] + 1; 
+				}else if(s1.charAt(i-1) != s2.charAt(j-1)) {
+					LCS[i][j] = (LCS[i-1][j] > LCS[i][j-1]) ? LCS[i-1][j] : LCS[i][j-1];
+				}
+			}
+		}
+		int max = 0;
+		
+		for(int i=0; i<s1Len+1; i++) {
+			for(int j=0; j<s2Len+1; j++) {
+				max = (LCS[i][j] > max) ? LCS[i][j] : max;
+			}
+		}
+		System.out.println(max);
+	}
 }
