da22f5f3d1f610a8bb2b7a63cd33d8f09728fe82
혜민 <97522726+heymin2@users.noreply.github.com>
2024-03-28 13:35:07 +0900

    1245_농장관리 코드 수정

--- 코테반/2회차/1245_농장관리/bj_1245_농장관리_신혜민.java
+++ 코테반/2회차/1245_농장관리/bj_1245_농장관리_신혜민.java
@@ -1,57 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, result = 0;
-    static List<Integer>[] list;
-    static boolean[] visit;
+    static int N, M, map[][];
+    static int[] dx = {1,1,1,-1,-1,-1,0,0};
+    static int[] dy = {0,1,-1,0,1,-1,-1,1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
 
-        N = Integer.parseInt(br.readLine());
-        list = new ArrayList[N+1];
-        visit = new boolean[N+1];
-
-        for(int i = 1; i <= N; i++) {
-            list[i] = new ArrayList<>();
-        }
-
-        for(int i = 0; i < N-1; i++) {
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-
-            list[a].add(b);
-            list[b].add(a);
-        }
-
-        visit[1] = true;
-        dfs(1, 0);
-
-        if(result % 2 == 0){
-            System.out.println("No");
-        }
-        else {
-            System.out.println("Yes");
-        }
-    }
-
-    static void dfs(int x, int cnt) {
-        for(int next : list[x]){
-            if(!visit[next]) {
-                visit[next] = true;
-                dfs(next, cnt+1);
+            for(int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        if(list[x].size() == 1 && x != 1) {
-            result += cnt;
+        boolean[][] visit = new boolean[N][M]; // 방문처리
+        boolean[][] t = new boolean[N][M]; // 산봉우리 체크
+        int result = 0;
+
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < M; j++) {
+                if(map[i][j] != 0 && !visit[i][j]) {
+                    boolean flag = true;
+                    Queue<Top> q = new ArrayDeque<>();
+
+                    q.offer(new Top(i, j));
+                    visit[i][j] = true;
+
+                    while(!q.isEmpty()) {
+                        Top now = q.poll();
+
+                        for(int k = 0; k < 8; k++) {
+                            int nx = now.x + dx[k];
+                            int ny = now.y + dy[k];
+
+                            if(nx < 0 || ny < 0 || nx >= N || ny >= M) continue;
+
+                            if(map[nx][ny] > map[now.x][now.y]){
+                                flag = false;
+                            } 
+
+                            if(map[nx][ny] == map[now.x][now.y]) {
+                                if(!visit[nx][ny]){
+                                    q.offer(new Top(nx, ny));
+                                    visit[nx][ny] = true;
+                                }
+                            }
+                        }
+                    }
+
+                    if(flag) {
+                        result++;
+                    }
+                }
+            }
         }
+        System.out.println(result);
+    }
+}
+
+class Top {
+    int x, y;
+
+    public Top(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
