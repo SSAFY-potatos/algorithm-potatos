fbf84ee8e4bcbc951e44b45f0198a5ce4a3b502b
sarmsoo <rnjsdydwls97@gmail.com>
2024-04-04 13:17:36 +0900

    yongjin-review: 2206_벽부수고이동하기, 11660_구간합구하기5, 2110_공유기설치, 15685_드래곤커브 풀이

--- 코테반/3회차/bj_11660_구간합구하기5_권용진.java
+++ 코테반/3회차/bj_11660_구간합구하기5_권용진.java
@@ -1,2 +1,53 @@
-public class bj_11660_구간합구하기5_조성우 {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int M;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		int[][] map = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < N; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		
+		int[][] dp = new int[N + 1][N + 1];
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N; j++) {
+				dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + map[i - 1][j - 1];
+			}
+		}
+		
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			
+			int maxY = Math.max(y1, y2);
+			int minY = Math.min(y1, y2);
+			int maxX = Math.max(x1, x2);
+			int minX = Math.min(x1, x2);
+			
+			int answer = dp[maxY][maxX]
+						 - dp[maxY][minX - 1] - dp[minY - 1][maxX] 
+						 + dp[minY - 1][minX - 1];
+			
+			sb.append(answer).append("\n");
+		}
+		System.out.println(sb);
+	}
 }
